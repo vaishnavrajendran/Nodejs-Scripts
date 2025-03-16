d95476e98c23ea02f7e62b96740503dd0632540f
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2025-03-16 15:31:08 +0530

    added reload also

--- week-27-docker-compose/src/index.ts
+++ week-27-docker-compose/src/index.ts
@@ -1,28 +1,26 @@
-import { PrismaClient } from "@prisma/client"
-import express from "express"
+import { PrismaClient } from "@prisma/client";
+import express from "express";
 
-const app = express()
+const app = express();
 const prismaClient = new PrismaClient();
 
 app.get("/", async (req, res) => {
-   
-    const data = await prismaClient.user.findMany();
-    res.json({
-        data
-    })
-})
+  const data = await prismaClient.user.findMany();
+  res.json({
+    data,
+  });
+});
 
 app.post("/", async (req, res) => {
-
-    await prismaClient.user.create({
-        data: {
-            username: Math.random().toString(),
-            password: Math.random().toString()
-        }
-    })
-    res.json({
-        "message": "post endpoint"
-    })
-})
+  await prismaClient.user.create({
+    data: {
+      username: Math.random().toString(),
+      password: Math.random().toString(),
+    },
+  });
+  res.json({
+    message: "post endpoint",
+  });
+});
 
 app.listen(3000);
